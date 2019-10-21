929fa12b6d895a66a8f533e5b7dd78bd0377a939
Joscha <joscha@plugh.de>
2019-10-21 11:58:32 +0000

    Do some basic setup

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import Rextra.Rextra (rextra)
 
 main :: IO ()
-main = someFunc
+main = rextra
---
+++ src/Rextra/Rextra.hs
@@ -0,0 +1,6 @@
+-- This file will be split up as it grows.
+module Rextra.Rextra where
+
+-- * Just a placeholder function
+rextra :: IO ()
+rextra = putStrLn "rextra"
